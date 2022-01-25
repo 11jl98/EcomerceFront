c6bc4d083e048185f864069a0e3c2c6811c1c71b
EriksJunior <EriksJunior13@gmail.com>
2022-01-24 22:35:53 -0300

    commit

--- src/components/Financial/Financial.vue
+++ src/components/Financial/Financial.vue
@@ -55,7 +55,55 @@
           </div>
         </div>
         <div id="containerContas">
-          <h3>TESTE</h3>
+          <b-tabs card>
+            <b-tab title="Cadastro de contas a receber / Pagar">
+              <b-card-text>
+                <div class="form-group col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                  <b-navbar toggleable class="cardDadosVendas">
+                    <b-navbar-toggle
+                      target="navbar-toggle-collapse3"
+                      id="dadosVendaStyle"
+                      class="tamanhoCards"
+                    >
+                      <template #default="{ expanded }">
+                        <div class="tamanhoBotaoOpenCard">
+                          <div>
+                            <b-icon
+                              v-if="expanded"
+                              icon="dash-square-fill"
+                              variant="info"
+                            ></b-icon>
+                            <b-icon
+                              v-else
+                              icon="plus-square-fill"
+                              variant="info"
+                            ></b-icon>
+                          </div>
+                          <div style="margin-left: 10px">Contas a Receber</div>
+                        </div>
+                      </template>
+                    </b-navbar-toggle>
+
+                    <b-collapse id="navbar-toggle-collapse3" is-nav>
+                      <b-navbar-nav class="ml-auto">
+                        <b-card
+                          class="
+                            mt-2
+                            col-md-12 col-sm-12 col-lg-12 col-xl-12
+                            cardDadosVendasBorda
+                          "
+                        >
+                          <b-card-text>
+                            <h3>3Bilhaum</h3>
+                          </b-card-text>
+                        </b-card>
+                      </b-navbar-nav>
+                    </b-collapse>
+                  </b-navbar>
+                </div>
+              </b-card-text>
+            </b-tab>
+          </b-tabs>
         </div>
       </div>
     </div>
@@ -81,13 +129,38 @@ export default {};
   margin-left: 6%;
 }
 
+#dadosVendaStyle {
+  border: none !important;
+}
+
 .titulo {
   width: 100%;
 }
+
 .titulo h3 {
   display: flex;
   margin-left: 35px;
 }
+
+.cardDadosVendasBorda {
+  border-radius: 5px !important;
+  margin-top: 25px !important;
+  margin-bottom: 20px;
+}
+
+.tamanhoCards {
+  width: 100%;
+  display: flex;
+}
+
+.tamanhoBotaoOpenCard {
+  color: black;
+  font-size: 16px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+}
+
 .cabecalho {
   width: 100%;
   height: auto;
