aefc6e950aad472f8408378584530fde571389ff
eriks junior <eriksjunior13@gmail.com>
2022-01-25 16:46:46 -0300

    commit

---
+++ src/components/Financial/Cadastro-Receber-Pagar.vue
@@ -0,0 +1,165 @@
+<template>
+  <b-navbar toggleable class="cardDadosContasReceberPagar">
+    <b-navbar-toggle
+      target="navbar-toggle-collapse"
+      id="dadosContasStyle"
+      class="tamanhoCardsContasReceberPagar"
+    >
+      <template #default="{ expanded }">
+        <div class="tamanhoBotaoOpenCardContas">
+          <div>
+            <b-icon v-if="expanded" icon="dash-square-fill"></b-icon>
+            <b-icon v-else icon="plus-square-fill"></b-icon>
+          </div>
+          <div style="margin-left: 10px">CONTAS A RECEBER</div>
+        </div>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-card
+          class="
+            mt-2
+            col-md-12 col-sm-12 col-lg-12 col-xl-12
+            cardDadosContasBorda
+          "
+        >
+          <div class="mt-4">
+            <b-row class="d-flex">
+              <b-form-input hidden class="col-sm-1"></b-form-input>
+
+              <b-form-group
+                id="input-group-1"
+                label="Tipo"
+                label-for="input-1"
+                class="col-sm-3"
+                size="sm"
+              >
+                <b-form-select
+                  v-model="dataBillReceive.tipo"
+                  text-field="tipo"
+                  value-field="id"
+                  :options="descriptionType"
+                ></b-form-select>
+              </b-form-group>
+
+              <b-form-group
+                id="input-group-1"
+                label="Cliente"
+                label-for="input-1"
+                class="col-sm-5"
+                size="sm"
+              >
+                <b-form-select
+                  v-model="selected"
+                  :options="options"
+                ></b-form-select>
+              </b-form-group>
+
+              <b-form-group
+                id="input-group-1"
+                label="Funcionario"
+                label-for="input-1"
+                class="col-sm-4"
+                size="sm"
+              >
+                <b-form-select
+                  v-model="selected"
+                  :options="options"
+                ></b-form-select>
+              </b-form-group>
+            </b-row>
+
+            <div class="mt-5">
+              <div class="d-flex justify-content-end">
+                <div>
+                  <b-button
+                    class="mr-4"
+                    size="sm"
+                    style="
+                      border: none !important;
+                      background-color: #56aafe !important;
+                    "
+                    >Salvar
+                    <b-icon-person-check class="ml-1"></b-icon-person-check
+                  ></b-button>
+
+                  <b-button class="mr-4" size="sm" variant="secondary">
+                    <b-icon-caret-down-fill></b-icon-caret-down-fill
+                    >Próximo</b-button
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </b-card>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import api from "../../services/axios";
+export default {
+  data() {
+    return {
+      dataBillReceive: {
+        id: "",
+        tipo: "",
+        idCliente: "",
+        idFuncionario: "",
+        idFormaPagamento: "",
+        idVenda: "",
+        valorTotal: 0,
+        valorPago: 0,
+        valorRestante: 0,
+        data: "",
+        dataPagamento: null,
+        descricao: "",
+      },
+      descriptionType: [{ tipo: "entrada", value: "id" }],
+    };
+  },
+  methods: {
+    async saveBill() {
+      const { data } = await api.post("");
+      console.log(data);
+    },
+  },
+};
+</script>
+
+<style>
+#dadosContasStyle {
+  border: none !important;
+}
+
+.tamanhoCardsContasReceberPagar {
+  width: 100% !important;
+  display: flex;
+  background-color: #6a88bd !important;
+  box-shadow: 2px 2px 4px 0px black;
+}
+
+.tamanhoBotaoOpenCardContas {
+  color: white;
+  font-size: 16px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+}
+
+.cardDadosContasBorda {
+  border-radius: 5px !important;
+  margin-top: 25px !important;
+  margin-bottom: 20px;
+  background-color: #6a88bd !important;
+  color: white;
+  box-shadow: 2px 2px 4px 0px black;
+}
+
+.cardDadosContasReceberPagar {
+  width: 50%;
+}
+</style>
--- src/components/Financial/Financial.vue
+++ src/components/Financial/Financial.vue
@@ -7,46 +7,42 @@
             <h3 class="mt-4 mb-4">Dashboard</h3>
           </div>
           <div class="containerCards">
-            <div class="cardsFinancas col-sm-12">
-              <div class="col-sm-3">
+            <div class="cardsFinancas col-sm-12 col-md-12 col-lg-12 col-xl-12">
+              <div class="col-sm-2">
                 <b-card
                   text-variant="dark"
                   sub-title="LUCRO (MENSAIS)"
-                  class="cardsLm p-2 col-sm-12"
-                  style="max-width: 17rem"
+                  class="cardsLm"
                 >
                   <b-card-text class="cardText"> R$2.000,00 </b-card-text>
                 </b-card>
               </div>
 
-              <div class="col-sm-3">
+              <div class="col-sm-2">
                 <b-card
                   text-variant="dark"
                   sub-title="LUCRO (ANUAL)"
-                  class="cardsLa p-2"
-                  style="max-width: 17rem"
+                  class="cardsLa"
                 >
                   <b-card-text class="cardText"> R$2.000,00 </b-card-text>
                 </b-card>
               </div>
 
-              <div class="col-sm-3">
+              <div class="col-sm-2">
                 <b-card
                   text-variant="dark"
                   sub-title="RECEBER (MENSAIS)"
-                  class="cardsRb p-2"
-                  style="max-width: 17rem"
+                  class="cardsRb"
                 >
                   <b-card-text class="cardText"> R$2.000,00 </b-card-text>
                 </b-card>
               </div>
 
-              <div class="col-sm-3">
+              <div class="col-sm-2">
                 <b-card
                   text-variant="dark"
                   sub-title="VENCER (MENSAIS)"
-                  class="cardsVc p-2"
-                  style="max-width: 17rem"
+                  class="cardsVc"
                 >
                   <b-card-text class="cardText"> R$2.000,00 </b-card-text>
                 </b-card>
@@ -54,64 +50,37 @@
             </div>
           </div>
         </div>
-        <div id="containerContas">
-          <b-tabs card>
-            <b-tab title="Cadastro de contas a receber / Pagar">
-              <b-card-text>
-                <div class="form-group col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                  <b-navbar toggleable class="cardDadosVendas">
-                    <b-navbar-toggle
-                      target="navbar-toggle-collapse3"
-                      id="dadosVendaStyle"
-                      class="tamanhoCards"
-                    >
-                      <template #default="{ expanded }">
-                        <div class="tamanhoBotaoOpenCard">
-                          <div>
-                            <b-icon
-                              v-if="expanded"
-                              icon="dash-square-fill"
-                              variant="info"
-                            ></b-icon>
-                            <b-icon
-                              v-else
-                              icon="plus-square-fill"
-                              variant="info"
-                            ></b-icon>
-                          </div>
-                          <div style="margin-left: 10px">Contas a Receber</div>
-                        </div>
-                      </template>
-                    </b-navbar-toggle>
-
-                    <b-collapse id="navbar-toggle-collapse3" is-nav>
-                      <b-navbar-nav class="ml-auto">
-                        <b-card
-                          class="
-                            mt-2
-                            col-md-12 col-sm-12 col-lg-12 col-xl-12
-                            cardDadosVendasBorda
-                          "
-                        >
-                          <b-card-text>
-                            <h3>3Bilhaum</h3>
-                          </b-card-text>
-                        </b-card>
-                      </b-navbar-nav>
-                    </b-collapse>
-                  </b-navbar>
-                </div>
-              </b-card-text>
-            </b-tab>
-          </b-tabs>
+        <div
+          id="containerContas"
+          class="col-sm-12 col-md-12 col-lg-12 col-xl-12 mt-5"
+        >
+          <div
+            class="containerCardsContas col-sm-12 col-md-12 col-lg-12 col-xl-12"
+          >
+            <b-card no-body class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+              <b-tabs card>
+                <b-tab title="Cadastro de contas a receber / Pagar">
+                  <div class="cardReceberPagar"><CadastroReceberPagar /></div>
+                </b-tab>
+                <b-tab title="Relatórios"> </b-tab>
+              </b-tabs>
+            </b-card>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
+
+
 <script>
-export default {};
+import CadastroReceberPagar from "../Financial/Cadastro-Receber-Pagar.vue";
+export default {
+  components: {
+    CadastroReceberPagar,
+  },
+};
 </script>
 
 <style>
@@ -138,33 +107,17 @@ export default {};
 }
 
 .titulo h3 {
-  display: flex;
   margin-left: 35px;
-}
-
-.cardDadosVendasBorda {
-  border-radius: 5px !important;
-  margin-top: 25px !important;
-  margin-bottom: 20px;
-}
-
-.tamanhoCards {
-  width: 100%;
-  display: flex;
-}
-
-.tamanhoBotaoOpenCard {
-  color: black;
-  font-size: 16px;
-  height: 30px;
-  display: flex;
-  align-items: center;
 }
 
 .cabecalho {
   width: 100%;
   height: auto;
   color: white;
+}
+
+.cardReceberPagar {
+  height: 300px !important;
 }
 
 .cardsLm {
@@ -175,19 +128,20 @@ export default {};
 
 .cardsLa {
   background-color: white !important;
-  border-left: 0.3rem solid #1cc88a !important;
+  border-left: 0.3rem solid #2b51a1 !important;
+
   border-radius: 0.5em !important;
 }
 
 .cardsRb {
   background-color: white !important;
-  border-left: 0.3rem solid #2b51a1 !important;
+  border-left: 0.3rem solid #1cc88a !important;
   border-radius: 0.5em !important;
 }
 
 .cardsVc {
   background-color: white !important;
-  border-left: 0.3rem solid #2b51a1 !important;
+  border-left: 0.3rem solid #df1818 !important;
   border-radius: 0.5em !important;
 }
 
@@ -196,9 +150,15 @@ export default {};
   display: flex;
   flex-wrap: wrap;
   height: auto;
+  justify-content: space-between;
+}
+
+.containerFinanceiro {
+  justify-content: end !important;
 }
 
 #containerContas {
-  margin-left: 35px;
+  width: 100%;
+  height: auto;
 }
 </style>
