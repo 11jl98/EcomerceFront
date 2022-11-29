6145a8169baf39fe54720eaea7672ec0f9d1449d
eriks junior <eriksjunior13@gmail.com>
2022-11-29 16:35:53 -0300

    refactor

--- src/components/Customer/Search.vue
+++ src/components/Customer/Search.vue
@@ -116,70 +116,10 @@
 </template>
 
 <script>
-import api from "../../services/axios";
-import toastAlertErros from "../../utils/toastAlertErros";
+import searchCustomer from "../../mixins/Customer/Search";
+
 export default {
-  data() {
-    return {
-      readCustomer: [],
-      textPesquisa: "",
-      tabIndex: 0,
-      page: 1,
-      dataLength: 0,
-      boolClear: false,
-    };
-  },
-  methods: {
-    nextPage() {
-      this.readCustomers((this.page += 1));
-    },
-
-    previousPage() {
-      if (this.page === 1) {
-        return;
-      } else {
-        this.readCustomers((this.page -= 1));
-      }
-    },
-
-    async readCustomers(page) {
-      try {
-        if (this.textPesquisa !== "") {
-          const { data } = await api.get(
-            `/customers?q=${this.textPesquisa}&page=${page}`
-          );
-          this.dataLength = data.data.length;
-          this.readCustomer = data.data;
-        } else {
-          return;
-        }
-      } catch (error) {
-        return this.$toast.open({
-          message: "Não foi possível listar os clientes",
-          type: "warning",
-        });
-      }
-    },
-    async editCustomer(customer) {
-      this.$emit("readOrEditCustomers", customer);
-      this.$root.$emit("bv::toggle::collapse", "accordion-dadosCadastrais");
-      this.$emit("alterTabIndex", this.tabIndex);
-    },
-    async destroyCustomer(idCustomer) {
-      try {
-        await api.delete(`/customers/${idCustomer}`);
-        this.readCustomers(this.page);
-        this.$emit("clearAll", this.boolClear);
-        this.boolClear = !this.boolClear;
-        return this.$toast.open({
-          message: "Cliente excluido com sucesso",
-          type: "success",
-        });
-      } catch (error) {
-        toastAlertErros.validateMessage(error, this.$toast);
-      }
-    },
-  },
+  mixins: [searchCustomer],
 };
 </script>
 <style scoped>
